2b2d407cbe67a63f6ff2986dc842dcf88be0170a
Anderson Grüdtner Martins <andergmartins@gmail.com>
2013-12-22 17:57:51 -0200

    Fix code style for content plugin

--- src/plg_content_osmeta/osmeta_content.php
+++ src/plg_content_osmeta/osmeta_content.php
@@ -11,76 +11,87 @@
  */
 
 // no direct access
-defined( '_JEXEC' ) or die( 'Restricted access' );
-$mainframe = JFactory::getApplication();
-$mainframe->registerEvent( 'onAfterContentSave',
-'pluginSeoBoss_onAfterContentSave' );
-$mainframe->registerEvent( 'onContentAfterSave',
-  'pluginSeoBoss_onContentAfterSave' );
+defined('_JEXEC') or die('Restricted access');
 
-function pluginSeoBoss_onAfterContentSave( $article, $isNew ){
-    $file = JPATH_ADMINISTRATOR."/components/com_seoboss/classes/ArticleMetatagsContainer.php";
-    if(is_object($article) &&
-	isset($article->id) && $article->id &&
-	isset($article->metakey) && $article->metakey &&
-        is_file( $file )) {
-        require_once( $file );
-        $ac = new ArticleMetatagsContainer();
-        $ac->saveKeywords($article->metakey, $article->id);
-    }
-  $db = JFactory::getDBO();
-  $db->setQuery ("SELECT enable_google_ping from  #__seoboss_settings " );
-  $settings = $db->loadObject();
-  if($settings->enable_google_ping){
-    $className = get_class($article);
+$app = JFactory::getApplication();
 
-    require_once JPATH_ADMINISTRATOR."/components/com_seoboss/classes/ExtensionsFactory.php";
-    $extensions = ExtensionsFactory::getExtensions();
+$app->registerEvent('onAfterContentSave', 'pluginSeoBoss_onAfterContentSave');
+$app->registerEvent('onContentAfterSave', 'pluginSeoBoss_onContentAfterSave');
 
-    if(is_array($extensions) && is_array($extensions['ping'])){
-      foreach( $extensions['ping'] as $pingHandler ){
-        if($pingHandler['class'] == $className){
-          require_once JPATH_ADMINISTRATOR."/components/com_seoboss/".$pingHandler['file'];
-          $url = '';
-          $rss = '';
-          if( isset($pingHandler['function']) &&
-            function_exists( $pingHandler['function'] ) ){
-            eval('$url='.$pingHandler['function'].'($article, $isNew);');
-          }
-          if( isset($pingHandler['rss_function']) &&
-            function_exists($pingHandler['rss_function'] ) ){
-            eval('$rss='.$pingHandler['rss_function'].'();');
-          }
-          if($url){
+function pluginSeoBoss_onAfterContentSave($article, $isNew)
+{
+	$file = JPATH_ADMINISTRATOR . "/components/com_seoboss/classes/ArticleMetatagsContainer.php";
+	if (is_object($article) && isset($article->id) && $article->id && isset($article->metakey) && $article->metakey && is_file($file))
+	{
+		require_once($file);
+		$ac = new ArticleMetatagsContainer;
+		$ac->saveKeywords($article->metakey, $article->id);
+	}
 
-            $db->setQuery("SELECT `domain` FROM `#__seoboss_settings`");
-            $domainName = $db->loadResult();
+	$db = JFactory::getDBO();
+	$db->setQuery("SELECT enable_google_ping from  #__seoboss_settings ");
+	$settings = $db->loadObject();
 
-            require_once JPATH_ADMINISTRATOR."/components/com_seoboss/classes/Pinger.php";
-            $config = JFactory::getConfig();
-            $pinger = new Pinger;
-            $result = $pinger->pingGoogle(
-              $config->get( 'config.sitename' ),
-              "http://$domainName",
-              "http://{$domainName}$url",
-              "http://{$domainName}$rss"
-            );
-            $db->setQuery("INSERT INTO #__seoboss_ping_status
-                                    (`date`, `title`, `url`, `response_code`, `response_text`) VALUES (
-                                    NOW(), ".$db->quote($article->title).", ".
-              $db->quote($url).", ".
-              $db->quote($result[0]).",".
-              $db->quote($result[1]).")");
-            $db->query();
-          }
-          break;
-        }
-      }
-    }
-  }
+	if ($settings->enable_google_ping)
+	{
+		$className = get_class($article);
+
+		require_once JPATH_ADMINISTRATOR . "/components/com_seoboss/classes/ExtensionsFactory.php";
+		$extensions = ExtensionsFactory::getExtensions();
+
+		if (is_array($extensions) && is_array($extensions['ping']))
+		{
+			foreach ($extensions['ping'] as $pingHandler)
+			{
+				if ($pingHandler['class'] == $className)
+				{
+					require_once JPATH_ADMINISTRATOR . "/components/com_seoboss/" . $pingHandler['file'];
+
+					$url = '';
+					$rss = '';
+
+					if (isset($pingHandler['function']) && function_exists($pingHandler['function']))
+					{
+						eval('$url=' . $pingHandler['function'] . '($article, $isNew);');
+					}
+
+					if (isset($pingHandler['rss_function']) && function_exists($pingHandler['rss_function']))
+					{
+						eval('$rss=' . $pingHandler['rss_function'] . '();');
+					}
+
+					if (!empty($url))
+					{
+						$db->setQuery("SELECT `domain` FROM `#__seoboss_settings`");
+						$domainName = $db->loadResult();
+
+						require_once JPATH_ADMINISTRATOR . "/components/com_seoboss/classes/Pinger.php";
+						$config = JFactory::getConfig();
+						$pinger = new Pinger;
+						$result = $pinger->pingGoogle(
+							$config->get('config.sitename'),
+							"http://$domainName",
+							"http://{$domainName}$url",
+							"http://{$domainName}$rss"
+						);
+
+						$db->setQuery("INSERT INTO #__seoboss_ping_status
+								(`date`, `title`, `url`, `response_code`, `response_text`) VALUES (
+								NOW(), " . $db->quote($article->title) . ", ".
+								$db->quote($url) . ", " .
+								$db->quote($result[0]) . "," .
+								$db->quote($result[1]) . ")");
+						$db->query();
+					}
+
+					break;
+				}
+			}
+		}
+	}
 }
 
-function pluginSeoBoss_onContentAfterSave($context, $article, $isNew){
-  pluginSeoBoss_onAfterContentSave($article, $isNew);
-
+function pluginSeoBoss_onContentAfterSave($context, $article, $isNew)
+{
+	pluginSeoBoss_onAfterContentSave($article, $isNew);
 }
