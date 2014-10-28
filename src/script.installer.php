d1e97e827db171eb32517134c43131c17df63969
Anderson Grüdtner Martins <anderson@ostraining.com>
2014-10-28 19:44:50 -0200

    Update related extensions to new builder/installer

--- src/script.installer.php
+++ src/script.installer.php
@@ -9,42 +9,18 @@
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
 
+$includePath = __DIR__ . '/admin/library/installer/include.php';
+if (file_exists($includePath)) {
+    require_once $includePath;
+} else {
+    require_once __DIR__ . '/library/installer/include.php';
+}
+
 /**
- * Metatags Container Factory Class
+ * OSMeta Installer Script
  *
  * @since  1.0
  */
-class com_OSMetaInstallerScript
+class Com_OSMetaInstallerScript extends AllediaInstallerAbstract
 {
-    /**
-     * method to run before an install/update/uninstall method
-     *
-     * @param  string  $type    type of change (install, update or discover_install)
-     * @param  string  $parent  the class calling this method
-     *
-     * @access  public
-     * @since   1.0
-     *
-     * @return void
-     */
-    public function preflight($type, $parent)
-    {
-        $jversion = new JVersion;
-        $jversion = $jversion->getShortVersion();
-
-        $manifest = $parent->get("manifest");
-
-        $this->release = (string) $manifest->version;
-        $this->minJoomlaRelease = (string) $manifest->attributes()->version;
-
-        // abort if the current Joomla release is older
-        if (version_compare($jversion, $this->minJoomlaRelease, 'lt')) {
-            $message = 'Cannot install com_osmeta in a Joomla release prior to ' . $this->minJoomlaRelease;
-            Jerror::raiseWarning(null, $message);
-
-            return false;
-        }
-
-        return true;
-    }
 }
