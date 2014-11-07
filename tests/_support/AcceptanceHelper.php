d2eb05534bb5bb73cd5fe23d6c50ead0ce892108
Anderson Grüdtner Martins <anderson@ostraining.com>
2014-11-07 14:48:05 -0200

    Add Acceptance tests for installer

--- tests/_support/AcceptanceHelper.php
+++ tests/_support/AcceptanceHelper.php
@@ -6,5 +6,53 @@ namespace Codeception\Module;
 
 class AcceptanceHelper extends \Codeception\Module
 {
+    protected $builtPackagePath;
 
+    protected $builtPackageFile;
+
+    public function buildProjectUsingPhing($task = 'build')
+    {
+        $output = shell_exec('phing ' . $task);
+        $this->debug("Builder output:\n" . $output);
+        preg_match(
+            '/Building zip\: (\/home\/vagrant\/Projects\/[a-z0-9_\-]*\/packages\/([a-z0-9_\-\.]*))/i',
+            $output,
+            $matches
+        );
+
+        if (!empty($matches)) {
+            $this->builtPackagePath = $matches[1];
+            $this->builtPackageFile = $matches[2];
+
+            $this->debug('Built file name: ' . $this->builtPackageFile);
+            $this->debug('Built file path: ' . $this->builtPackagePath);
+
+            // Move the file to the _data dir
+            // $dataPath = 'tests/_data/' . $this->builtPackageFile;
+            // $copied = copy($this->builtPackagePath, $dataPath);
+            // $this->debug('Copied package to: ' . $dataPath);
+        }
+
+        return $output;
+    }
+
+    public function getBuiltFilePath()
+    {
+        return $this->builtPackagePath;
+    }
+
+    public function getBuiltFile()
+    {
+        return $this->builtPackageFile;
+    }
+
+    public function unzipBuiltPackageToTmp()
+    {
+        $tmpPath = '/var/www/osmeta/tmp/test_' . uniqid();
+
+        shell_exec("unzip {$this->builtPackagePath} -d {$tmpPath}");
+        $this->debug('Unzipping package to: ' . $tmpPath);
+
+        return $tmpPath;
+    }
 }
---
+++ tests/acceptance/InstallerCest.php
@@ -0,0 +1,47 @@
+<?php
+use \AcceptanceTester;
+
+class InstallerCest
+{
+    protected $packagePath = '';
+
+    public function _before(AcceptanceTester $I)
+    {
+        $I->resizeWindow(1280, 900);
+
+        // Build a new package
+        $I->buildProjectUsingPhing();
+
+        // Unzip the built package
+        $this->packagePath = $I->unzipBuiltPackageToTmp();
+
+        // Opens the administrator page to login
+        $I->amOnPage('/administrator');
+        $I->fillField('username', 'admin');
+        $I->fillField('passwd', 'admin');
+        $I->click('Log in');
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+
+    }
+
+    public function installPackage(AcceptanceTester $I)
+    {
+        // Goes to the installer page
+        $I->amOnPage('administrator/index.php?option=com_installer');
+
+        // Click Install from Directory
+        $I->click('//*[@id="myTabTabs"]/li[3]/a');
+
+        $I->fillField('install_directory', $this->packagePath);
+        $I->click('//*[@id="directory"]/fieldset/div[2]/input');
+        $I->makeScreenshot('post_install');
+
+        $I->see('Thanks for');
+        $I->see('OSMeta');
+        $I->see('Show more details...');
+        $I->dontSee('Error');
+    }
+}
