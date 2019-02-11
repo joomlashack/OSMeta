44a8e9f17f85817074c1eb824ea94a80fb30c308
Bill Tomczak <bill@joomlashack.com>
2019-02-11 13:50:55 -0800

    Update file headers

--- src/admin/views/osmeta/tmpl/default.php
+++ src/admin/views/osmeta/tmpl/default.php
@@ -2,11 +2,25 @@
 /**
  * @package   OSMeta
  * @contact   www.joomlashack.com, help@joomlashack.com
- * @copyright 2013-2016 Open Source Training, LLC, All rights reserved
- * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
+ * @copyright 2013-2019 Joomlashack.com. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ *
+ * This file is part of OSMeta.
+ *
+ * OSMeta is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * OSMeta is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OSMeta.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-// No direct access
 defined('_JEXEC') or die();
 
 $colspan = $this->extension->isPro() ? 5 : 4;
--- src/extensions/OSMetaContent/osmetacontent.php
+++ src/extensions/OSMetaContent/osmetacontent.php
@@ -2,15 +2,29 @@
 /**
  * @package   OSMeta
  * @contact   www.joomlashack.com, help@joomlashack.com
- * @copyright 2013-2016 Open Source Training, LLC, All rights reserved
- * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
+ * @copyright 2013-2019 Joomlashack.com. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ *
+ * This file is part of OSMeta.
+ *
+ * OSMeta is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * OSMeta is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OSMeta.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 use Alledia\Framework\Joomla\Extension;
 use Alledia\Framework;
 use Alledia\OSMeta;
 
-// No direct access
 defined('_JEXEC') or die();
 
 include_once JPATH_ADMINISTRATOR . '/components/com_osmeta/include.php';
